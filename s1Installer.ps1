f1fbe80bbd5f605e95076714181949426221037a
ahinkson-cloudscale <49921569+ahinkson-cloudscale@users.noreply.github.com>
2025-03-28 15:35:36 -0400

    Delete s1Installer.ps1

--- s1Installer.ps1
+++
@@ -1,9 +0,0 @@
-$SiteToken = Read-Host "Enter your SentinelOne Site Token"
-$DownloadUrl = "https://usea1-swprd1.sentinelone.net/web/api/v2.1/update/agent/download/1954575331355041287/2102955481874601430"
-$InstallerPath = "$env:TEMP\SentinelInstaller.msi"
-
-# Download the MSI installer
-Invoke-WebRequest -Uri $DownloadUrl -OutFile $InstallerPath
-
-# Install SentinelOne silently
-Start-Process "msiexec.exe" -ArgumentList "/i `"$InstallerPath`" /q SITE_TOKEN=$SiteToken WSC=false" -Wait -NoNewWindow
